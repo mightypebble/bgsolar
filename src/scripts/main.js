6c8a3635a9d3113b83ba3b09d9f28746eb523ecb
mightypebble <pebblevideos@gmail.com>
2022-09-14 19:08:26 +0300

    Finished contacts section
    Added functioning form with toggle buttons for mobile and fixed grid

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,13 +2,16 @@ import $ from './libs/jquery.min';
 import Header from './modules/header';
 import Home from './modules/home';
 import Products from './modules/products';
+import Contacts from './modules/contacts';
 
 $(document).ready(() => {
     const header = document.querySelector('.header');
     const home = document.querySelector('.home');
     const products = document.querySelector('.products');
+    const contacts = document.querySelector('.contacts');
 
     const headerInstance = new Header(header);
     const homeInstance = new Home(home);
     const productsInstance = new Products(products);
+    const contactsInstance = new Contacts(contacts)
 });
